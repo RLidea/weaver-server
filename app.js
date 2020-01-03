1cb88c735a0ee4cb33d943e2d00be790f69559a6
Centell <centell@me.com>
2020-01-03 18:53:47 +0900

    add error page

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 require('dotenv').config();
 
 const express = require('express');
-// const createError = require('http-errors');
+const createError = require('http-errors');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
@@ -63,5 +63,27 @@ if (process.env.NODE_ENV == 'development') {
 app.use('/', require('./routes/index'));
 app.use('/users', require('./routes/users'));
 app.use('/auth', require('./routes/auth'));
+
+/*
+ * Error Handler
+ */
+
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+app.use(function(err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODE_ENV === 'production' ? { status: err.status } : err;
+
+  console.log('[system] An error has occurred from client');
+  console.log(res.locals.error);
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error', {
+    title: '::' + err.status,
+  });
+});
 
 module.exports = app;
