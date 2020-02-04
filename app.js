5f53916244d46d2b315d4e87a1d5185f5bcebe27
Centell <centell@me.com>
2020-02-04 15:54:23 +0900

    add: i18n

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ const passportConfig = require('./app/controllers/auth/passport');
 const csrf = require('csurf');
 const cors = require('cors');
 const ejsLocals = require('ejs-locals');
+const i18next = require('i18next');
+const i18nextMiddleware = require('i18next-express-middleware');
+const i18nextFsBackend = require('i18next-node-fs-backend');
 
 /*
  * Environment Configurations
@@ -36,6 +39,30 @@ passportConfig();
 app.use(cors(require('./app/middleware/CORS')));
 
 /*
+ * Middleware
+ */
+
+// logger
+const logger = require('./app/middleware/Logger');
+app.use(logger.printTerminalDev);
+app.use(process.env.NODE_ENV == 'development' ? logger.saveFileDev : logger.saveFileDefault);
+
+// i18n
+i18next
+  .use(i18nextFsBackend)
+  .use(i18nextMiddleware.LanguageDetector)
+  .init({
+    backend: {
+      loadPath: __dirname + '/locales/{{lng}}.json',
+      addPath: __dirname + '/locales/{{lng}}.missing.json',
+    },
+    fallbackLng: 'en',
+    preload: ['en', 'ko'],
+    saveMissing: true,
+  });
+app.use(i18nextMiddleware.handle(i18next));
+
+/*
  * Routers
  */
 
@@ -46,18 +73,12 @@ app.use('/users', require('./routes/users'));
 app.use('/admin', require('./routes/admin'));
 
 /*
- * Middleware
+ * Error
  */
-
-// logger
-const logger = require('./app/middleware/Logger');
-app.use(logger.printTerminalDev);
-app.use(process.env.NODE_ENV == 'development' ? logger.saveFileDev : logger.saveFileDefault);
-
-// error
 const error = require('./app/middleware/Error');
 app.use(error.notFoundError);
 app.use(error.errorMessage);
 
 console.log(`[System] ${process.env.APP_NAME} is ready`);
+
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -13,4 +13,14 @@ router.get('/not_allowed', (req, res, next) => {
   res.render('not_allowed', { title: process.env.APP_NAME });
 });
 
+router.get('/en', (req, res) => {
+  req.i18n.changeLanguage('en');
+  res.send(req.t('admin.demo'));
+});
+
+router.get('/ko', (req, res) => {
+  req.i18n.changeLanguage('ko');
+  res.send(req.t('admin.demo'));
+});
+
 module.exports = router;
