1f5b4f66cb807e57b0ad4205cb41e7e64cf768e0
Centell <centell@me.com>
2020-04-28 19:14:01 +0900

    add: documetns list

--- app/controllers/BoardController.js
+++ app/controllers/BoardController.js
@@ -1,9 +1,7 @@
 const Model = require('@models');
 
-const BoardModel = Model.board;
-
-module.exports.boardList = async () => {
-  const boards = await BoardModel.findAll({
+const boardList = async () => {
+  const boards = await Model.board.findAll({
     where: {
       is_use: true,
     },
@@ -11,4 +9,28 @@ module.exports.boardList = async () => {
     return item.dataValues;
   }));
   return boards;
+};
+
+const documentList = async (req, res, next) => {
+  const { boards_id } = req.query;
+
+  const list = await Model.document.findAll({
+    where: {
+      boards_id,
+      deleted_at: null,
+    },
+    order: [
+      ['order', 'DESC'],
+      ['id', 'DESC'],
+    ],
+  })
+    .then(d => d)
+    .catch(e => e);
+
+  return res.json(list);
+};
+
+module.exports = {
+  boardList,
+  documentList,
 };
--- routes/apis.js
+++ routes/apis.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const BoardController = require('@controllers/BoardController');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
   return res.send('api!');
 });
+
+router.route('/board').get(BoardController.documentList);
 
 module.exports = router;
