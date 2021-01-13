e0303604c87d00b90182297a4034b956f3f3118f
Centell <centell@me.com>
2021-01-13 11:51:52 +0900

    :wrench: add list size config

--- src/app.js
+++ src/app.js
@@ -32,8 +32,13 @@ app.engine('ejs', ejsLocals);
 /*
  * Express Configurations
  */
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({
+  limit: '50mb',
+}));
+app.use(express.urlencoded({
+  limit: '50mb',
+  extended: false,
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(passport.initialize());
