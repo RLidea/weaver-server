e5f5a4bbd726765c65eef13c1fdf07f013a154a0
Centell <centell@me.com>
2021-04-20 18:53:57 +0900

    :recycle: refactor utils

--- src/app/utils/formatter.js
+++ src/app/utils/formatter.js
@@ -1,10 +1,13 @@
+const fs = require('fs');
+
+const util = {};
+
 /**
  * 자료구조 변환: 리스트 -> 트리
  * @param list
  * @returns {[]}
  */
-
-const listToTree = list => {
+util.listToTree = list => {
   const map = {};
   const roots = [];
 
@@ -27,14 +30,27 @@ const listToTree = list => {
   return roots;
 };
 
-const toNumber = value => {
+util.toNumber = value => {
   const result = Number(value);
   if (value === null) return null;
   if (Number.isNaN(result)) return undefined;
   return result;
 };
 
-module.exports = {
-  listToTree,
-  toNumber,
+/*
+ base64 handler
+ */
+util.imageFileToBase64String = filePath => {
+  const buff = fs.readFileSync(filePath);
+  return buff.toString('base64');
 };
+
+util.base64StringToImageFile = (dataString, filePath) => {
+  const buff = Buffer.from(dataString, 'base64');
+  fs.writeFileSync(filePath, buff);
+  return {
+    filePath,
+  };
+};
+
+module.exports = util;
