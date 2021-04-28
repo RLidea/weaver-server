6a77d9e87268aa14e345418accc1ac0cc5646f53
Centell <centell@me.com>
2021-04-28 11:41:32 +0900

    add config.js

--- src/system/logger.js
+++ src/system/logger.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const winston = require('winston');
 const WinstonDaily = require('winston-daily-rotate-file');
+const config = require('@src/config');
 
 // log directory
 const varDir = `${__dirname}/../../var`;
